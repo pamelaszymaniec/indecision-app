773129fc26e713530cfb8a67edf0df138477a4db
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2017-11-23 16:28:53 +0100

    task3/21: buildit: visibility toggle

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,26 +1,27 @@
-let visibility = false;
 
+
+let visibility = false;
 const toggleVisibility = () => {
-  visibility = !visibility;
-  render();
+    visibility = !visibility;
+    render();
 };
 
-const render = () => {
-  const jsx = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggleVisibility}>
-        {visibility ? 'Hide details' : 'Show details'}
-      </button>
-      {visibility && (
-        <div>
-          <p>Hey. These are some details you can now see!</p>
-        </div>
-      )}
-    </div>
-  );
 
-  ReactDOM.render(jsx, document.getElementById('app'));
+const render = () => {
+    const jsx = (
+        <div>
+            <h1>Visibilty Toggle</h1>
+
+            <button onClick={toggleVisibility}>{visibility ? 'Hide details' : 'Show details'}</button>
+            {visibility && (
+                <div>
+                    <p>Hey. These are some details you can now see!</p>
+                </div>
+            )}
+        </div>
+    );
+
+    ReactDOM.render(jsx, document.getElementById('app'));
 };
 
 render();
