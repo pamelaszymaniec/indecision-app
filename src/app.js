1b88c0038ae5580bbf608b1bf3c74600b54f68bf
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2017-11-23 16:11:13 +0100

    task3/20: picking an option

--- src/app.js
+++ src/app.js
@@ -22,6 +22,14 @@ const onRemoveAll = () => {
     app.options = [];
     render();
 };
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random()*app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+
+};
+
 const appRoot = document.getElementById('app');
 
 const render = () => {
@@ -30,9 +38,7 @@ const render = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <p>{app.options.length}</p>
-
-
+            <button disabled={app.options.length ===0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={onRemoveAll}>Remove All</button>
             <ol>
                 {
