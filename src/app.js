b8f84653ad0dcf6daed964ea5b3ce4c5d09b7b5e
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2017-11-24 12:26:32 +0100

    task4/28: component props

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-class  IndecisionApp extends React.Component {
+class IndecisionApp extends React.Component {
     render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of computer';
+        const options = ['thing one', 'thing two', 'thing three'];
+
         return (
             <div>
-                <Header/>
+                <Header title={title} subtitle={subtitle}/>
                 <Action/>
-                <Options />
+                <Options options={options}/>
                 <AddOption/>
 
             </div>
@@ -13,14 +17,14 @@ class  IndecisionApp extends React.Component {
 }
 
 
-
 class Header extends React.Component {
     render() {
+
         return (
             <div>
-                <h1>Indecision</h1>
+                <h1>{this.props.title}</h1>
 
-                <h2>Put your life in the hands of computer</h2>
+                <h2>{this.props.subtitle}</h2>
             </div>
 
         )
@@ -42,8 +46,13 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                <p>Options component here</p>
-                <Option />
+                {
+                    this.props.options.map((option) =>
+
+                        <Option key={option} optionText={option}/>
+                    )
+                }
+                <Option/>
             </div>
         )
     }
@@ -53,13 +62,12 @@ class Option extends React.Component {
     render() {
         return (
             <div>
-                <p>hello option one</p>
+               Option: {this.props.optionText}
             </div>
         )
-}
+    }
 
 }
-
 
 
 class AddOption extends React.Component {
@@ -74,8 +82,6 @@ class AddOption extends React.Component {
         )
     }
 }
-
-
 
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
