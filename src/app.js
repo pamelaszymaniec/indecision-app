22eb55c88b60c6ef3df3c88e1b292c7a0dbbf0cb
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-07-10 13:41:24 +0200

    task6/58: new syntax - es6 class properties

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp2';
 
-
-
 ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+
+class OldSyntax {
+    constructor() {
+        this.name = 'Mike';
+    }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+// ------------
+
--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 export default class AddOption extends React.Component {
+    state = {
+      error: undefined
+    }
     constructor(props) {
         super(props);
         this.handleAddOption = this.handleAddOption.bind(this);
-        this.state = {
-            error: undefined
-        }
+
     }
 
-    handleAddOption(e) {
+    handleAddOption = (e) => {
         e.preventDefault();
         console.log('testing');
         const option = e.target.elements.option.value.trim();
