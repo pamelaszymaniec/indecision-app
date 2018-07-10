5b69e512d4034df6fd9c9be8cbdf2ec4d8429d3c
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2018-07-10 10:58:15 +0200

    task6/52-53: importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
-// import './utils.js'
+import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import substract, { square, add } from "./utils.js";
-//
-// console.log('app.js is running!!');
-// console.log(square(4));
-// console.log(add(100,23));
-// console.log(substract(100,81));
-
-
-import isSenior, {isAdult, canDrink} from "./person.js";
-const age = 24;
-console.log(isAdult(age));
-console.log(canDrink(age));
-console.log(isSenior(65));
+const template = React.createElement('p', {}, 'test');
+ReactDOM.render(template, document.getElementById('app'));
